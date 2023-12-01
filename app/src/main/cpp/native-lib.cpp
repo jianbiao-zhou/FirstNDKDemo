a6e2f09363ce6710a6119f03090b0fcbe5fe5913
jianbiao.zhou <jianbiao.zhou@mxplayer.in>
2023-12-01 19:28:04 +0800

    [ZJB]Added support of android log in cpp.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,12 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_firstndkdemo_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    __android_log_print(ANDROID_LOG_DEBUG, "JNI","print a log in cpp.");
     return env->NewStringUTF(hello.c_str());
 }
